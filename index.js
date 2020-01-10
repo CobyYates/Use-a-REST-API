a3e1e7f8cf1c297f1242040b6f7ec25f635835e0
Coby Yates <cobylyates@gmail.com>
2020-01-09 17:11:40 -0700

    working rest api call, displays cards, adds to favorites, add new card all working

--- index.js
+++ index.js
@@ -2,95 +2,88 @@ async function getAPIData(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data)
     return data;
   } catch (error) {
     console.error(error);
   }
 }
 
-const theData = getAPIData('https://uinames.com/api/?ext&region=united states&amount=10').then(data => {
-  let card = document.querySelector("#root")
+class Person {
+  constructor(name, surname, email, age, phone, birthday, photo) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.age = age;
+    this.phone = phone;
+    this.birthday = birthday;
+    this.photo = photo;
+  }
+}
 
-  card.textContent = data[0].name
-  
-})
+const NewPerson = new Person(
+  "Kevin",
+  "Hart",
+  "kevin.hart@comedy.com",
+  40,
+  "(123) 456 7890",
+  { mdy: "06/19/1987" },
+  "https://media.todaybirthdays.com/upload/2015/11/29/kevin-hart.jpg"
+);
 
-// const ul = document.getElementById("userList");
-// let page = 1;
+let mainArea = document.querySelector("main");
 
-// const url = `https://randomuser.me/api/?results=10&seed=uvu${page}`;
+const theData = getAPIData(
+  `https://uinames.com/api/?ext&region=united states&amount=15`
+).then(data => {
+  data.map(user => {
+    populateDOM(user);
+  });
+  addButton();
+});
 
-// const nextBtn = document.getElementById("nextBtn");
+function addButton() {
+  let btn = document.createElement("button");
+  btn.setAttribute("class", "btn");
+  btn.textContent = "ADD";
+  mainArea.appendChild(btn);
 
-// nextBtn.onclick = function() {
-//   ul.innerHTML = "";
-//   page++;
+  btn.addEventListener("click", function() {
+    populateDOM(NewPerson);
+  });
+}
 
-//   console.log(url);
-//   console.log(url);
+function populateDOM(single_user) {
+  let card = document.createElement("div"),
+    name = document.createElement("p"),
+    email = document.createElement("p"),
+    age = document.createElement("p"),
+    phone = document.createElement("p"),
+    img = document.createElement("img"),
+    add = document.createElement("button")
 
-//   fetch(url + "&page=" + page)
-//     .then(resp => resp.json())
-//     .then(data => {
-//       const users = data.results;
+  name.textContent = `Name: ${single_user.name} ${single_user.surname}`;
+  email.textContent = `Email: ${single_user.email}`;
+  age.textContent = `Age: ${single_user.age}`;
+  phone.textContent = `Phone: ${single_user.phone}`;
+  add.textContent = `Add ${single_user.name} to Favorites`
+  img.src = single_user.photo;
 
-//       // users.forEach(user => {
-//       //   console.log(user)
-//       // });
-//       // or
+  card.appendChild(img);
+  card.appendChild(name);
+  card.appendChild(age);
+  card.appendChild(phone);
+  card.appendChild(email);
+  card.appendChild(add);
+  mainArea.appendChild(card);
 
-//       users.map(user => {
-//         // console.log(user)
+  add.addEventListener("click", function() {
+    var x = document.querySelector(".sBar1");
+    x.textContent = `${single_user.name} added`
+    // x.className = "show";
+    x.classList.toggle("show")
+    setTimeout(function() {
+      x.classList.toggle("show")
+    }, 3000);
+  })
+}
 
-//         let img = createEl("img"),
-//           p = createEl("p");
-//         li = createEl("li");
-
-//         img.src = user.picture.thumbnail;
-//         p.innerHTML = `${user.name.first} ${user.name.last}`;
-
-//         li.appendChild(img);
-//         li.appendChild(p);
-
-//         ul.appendChild(li);
-//       });
-//     });
-// };
-
-// let users = fetch(url);
-
-// users
-//   .then(function(resp) {
-//     return resp.json();
-//   })
-//   .then(function(data) {
-//     // console.log(data);
-
-//     const users = data.results;
-
-//     // users.forEach(user => {
-//     //   console.log(user)
-//     // });
-//     // or
-
-//     users.map(user => {
-//       // console.log(user)
-
-//       let img = createEl("img"),
-//         p = createEl("p"),
-//         li = createEl("li")
-
-//       img.src = user.picture.thumbnail;
-//       p.innerHTML = `${user.name.first} ${user.name.last}`;
-
-//       li.appendChild(img);
-//       li.appendChild(p);
-
-//       ul.appendChild(li);
-//     });
-//   });
-
-// function createEl(el) {
-//   return document.createElement(el);
-// }
